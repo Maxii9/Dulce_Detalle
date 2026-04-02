b0d768e178238b62da409704f3f5fa502dfa887d
Maxi <maxibenjpibernus@gmail.com>
2026-04-02 09:59:50 -0300

    Creacion Super Usuario corregido

--- dulce_detalle/build.sh
+++ dulce_detalle/build.sh
@@ -6,3 +6,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if environment variables are present
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser \
+    --no-input \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL" || true
+fi
